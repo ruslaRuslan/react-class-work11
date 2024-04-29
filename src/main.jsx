e51a6527950dcf2f329fcc4f964284c071a1d3c0
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-29 15:51:23 +0400

    store yaratdiq

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 ReactDOM.createRoot(document.getElementById("root"))
 
-const store = 
+const store = createStore()
+// deprecated - kohnelmis
 createRoutesFromElements.render(
   <BrowserRouter>
     <Provider store={store}>
